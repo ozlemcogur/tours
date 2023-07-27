be031ffceed100555aa4357e8f46a030001dea68
ozlemcogur <fatmaozlemcogur@gmail.com>
2023-07-27 17:14:37 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const menuBtn = document.querySelector('.navbar-menu-btn')
+
+
+const menuLinks = document.querySelector('.navbar-links')
+
+
+menuBtn.addEventListener('click', () => {
+    menuLinks.classList.toggle('mobile-menu')
+})
